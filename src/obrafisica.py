19a6aa67498d153288564de7c361924da1bcfb8a
ramonraydan <96272006+ramonraydan@users.noreply.github.com>
2024-06-10 23:27:11 -0300

    Update obrafisica.py

--- src/obrafisica.py
+++ src/obrafisica.py
@@ -1,9 +1,9 @@
 class ObraFisica:
   def __init__(self, titulo, autor, data_publicacao, paginas, quantidade, exemplar, estado):
       self._titulo = titulo
-      self.autor = autor
-      self.data_publicacao = data_publicacao
-      self.paginas = paginas
-      self.quantidade = quantidade
-      self.exemplar = exemplar
-      self.estado = estado
+      self._autor = autor
+      self._data_publicacao = data_publicacao
+      self._paginas = paginas
+      self._quantidade = quantidade
+      self._exemplar = exemplar
+      self._estado = estado
