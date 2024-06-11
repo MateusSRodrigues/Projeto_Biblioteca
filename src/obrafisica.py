694a37a21771f3de231ac28fe83eb76177359b2a
ramonraydan <96272006+ramonraydan@users.noreply.github.com>
2024-06-10 23:22:55 -0300

    construtor obras fisicas

---
+++ src/obrafisica.py
@@ -0,0 +1,9 @@
+class ObraFisica:
+  def __init__(self, titulo, autor, data_publicacao, paginas, quantidade, exemplar, estado):
+      self._titulo = titulo
+      self.autor = autor
+      self.data_publicacao = data_publicacao
+      self.paginas = paginas
+      self.quantidade = quantidade
+      self.exemplar = exemplar
+      self.estado = estado
