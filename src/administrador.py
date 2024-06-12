5b80cf2422cc5db2314d2ba499b4a93d19ea461f
ramonraydan <96272006+ramonraydan@users.noreply.github.com>
2024-06-12 18:43:52 -0300

    menu adm

--- src/administrador.py
+++ src/administrador.py
@@ -1,45 +1,60 @@
 from pessoa import Pessoa
 import os,termios, sys, tty
+import time     #apenas pra para a impressao final, retirar apos imlementar o que cada opçao faz!!!!!!!
 
 class Administrador(Pessoa):
-    def __init__():super().__init__(nome, email, cpf, endereco, saldo, senha)
-        def get_char():
-    # Função para capturar o caractere pressionado pelo usuário
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
-    try:
-        tty.setraw(sys.stdin.fileno())
-        ch = sys.stdin.read(1)
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-    return ch
+    def __init__(self, nome, email, cpf, endereco, saldo, senha):super().__init__(nome, email, cpf, endereco, saldo, senha)
+   
+    def menu(self):
+        def get_char():        # Função para capturar o caractere pressionado pelo usuário
+            fd = sys.stdin.fileno()
+            old_settings = termios.tcgetattr(fd)
+            try:
+                tty.setraw(sys.stdin.fileno())
+                ch = sys.stdin.read(1)
+            finally:
+                termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+            return ch
+        c = 0
+        while True:
+            opcao = ["Fazer reserva", "Fazer emprestimo", "Fazer devolucao", "Calcular multa","Deletar usuario"]
+            cabecalho = ["Pressione 'W' para subir 'S' para descer e 'D' para selecionar.\n", "O que deseja Fazer?"]
+            os.system('clear')
+            os.system('cls' if os.name == 'nt' else 'clear')        
+    
+            for i in range(len(opcao)):
+                if i == 0:
+                    for item in cabecalho:
+                        print(item, end='')
+                    print("\n------------------------------")
+                if i == c % len(opcao):
+                    print(f"->{opcao[i]}")
+                else:
+                    print(opcao[i])
+                if i == len(opcao) - 1:
+                    print("====================================\n====================================")
+    
+            n = get_char()
+    
+            if n == 's':
+                c += 1  # altera contador de click, desce a seta
+            elif n == 'w':
+                c -= 1  # altera contador de click, sobe a seta
+            elif n == 'd':  # escolhe opção
+                if c == 0:
+                    if opcao[c] == opcao[0]:
+                        print('esta é a opcao 1')
+                        time.sleep(1000)
 
-def menu2(opcao, cabecalho):
-    c = 0
-    while True:
-        os.system('clear')
-        os.system('cls' if os.name == 'nt' else 'clear')        
-
-        for i in range(len(opcao)):
-            if i == 0:
-                for item in cabecalho:
-                    print(item, end='')
-                print("\n------------------------------")
-            if i == c % len(opcao):
-                print(f"->{opcao[i]}")
-            else:
-                print(opcao[i])
-            if i == len(opcao) - 1:
-                print("====================================\n====================================")
-
-        n = get_char()
-
-        if n == 's':
-            c += 1  # altera contador de click, desce a seta
-        elif n == 'w':
-            c -= 1  # altera contador de click, sobe a seta
-        elif n == 'd':  # escolhe opção
-            if c == 0:
-                return opcao[0]
-            else:
-                return opcao[c % len(opcao)]
+                else:                     #alterar o que cada opçao faz
+                    if opcao [c % len(opcao)] == opcao[0]:
+                        print('esta é a opcao ' + opcao[0])
+                    if opcao [c % len(opcao)] == opcao[1]:
+                        print('esta é a opcao ' + opcao[1])
+                    if opcao [c % len(opcao)] == opcao[2]:
+                        print('esta é a opcao ' + opcao[2])
+                    if opcao [c % len(opcao)] == opcao[3]:
+                        print('esta é a opcao ' + opcao[3])
+                    if opcao [c % len(opcao)] == opcao[4]:
+                        print('esta é a opcao ' + opcao[4])
+                time.sleep(1000)
