6743c12c01460ebdd76beeba0d97b5be0f06b551
ramonraydan <96272006+ramonraydan@users.noreply.github.com>
2024-06-12 16:31:01 -0300

    constr

--- src/administrador.py
+++ src/administrador.py
@@ -1,5 +1,4 @@
 from pessoa import Pessoa
 
 class Administrador(ObraFisica):
-    def __init__(__init__(nome, email, cpf, endereco, saldo, senha)
-      
+    def __init__():super().__init__(nome, email, cpf, endereco, saldo, senha)
