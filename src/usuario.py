34fd14015709e4417c8fe38b031f564167cad9d9
ramonraydan <96272006+ramonraydan@users.noreply.github.com>
2024-06-12 23:33:34 -0300

    Update usuario.py

---
+++ src/usuario.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+
+class Usuario:
+  
+  @abstractmethod
+  deletarUsuario(self):
+    pass
+
+  @abstractmethod
+  verificarMulta():
+    pass
+    
+  @abstractmethod
+  pedirReserva():
+    pass
