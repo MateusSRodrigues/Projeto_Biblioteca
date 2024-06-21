9ef2200357a15d8af272fa1fe095464e4b80d18c
ramonraydan <96272006+ramonraydan@users.noreply.github.com>
2024-06-21 16:20:52 -0300

    Update pessoa.py

--- src/pessoa.py
+++ src/pessoa.py
@@ -3,7 +3,7 @@ class Pessoa:
         if nome is None or email is None or cpf is None or endereco is None or senha is None:
             self._sup_interactive()
         else:
-            self.nome = 
+            self._nome = nome
             self._email = email
             self._cpf = cpf
             self._endereco = endereco
@@ -69,3 +69,46 @@ class Pessoa:
                     raise ValueError("A senha não pode ser vazia.")
             except ValueError as e:
                 print(e)
+
+    @property
+    def nome(self):
+        return self._nome
+    
+    @id.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome
+        @property
+        
+    def email(self):
+        return self._email
+    
+    @id.setter
+    def email(self, novo_email):
+        self._email = novo_email
+        
+    @property
+    def cpf(self):
+        return self._cpf
+    
+    @id.setter
+    def cpf(self, novo_cpf):
+        self._cpf = novo_cpf
+        
+    @property   
+    def endereco(self):
+        return self._endereco
+    
+    @id.setter
+    def endereco(self, novo_endereco):
+        self._endereco = novo_emendereco
+        
+    @property   
+    def senha(self):
+        return self._senha
+    
+    @id.setter
+    def senha(self, nova_senha):
+        self._senha = nova_senha
+
+
+
